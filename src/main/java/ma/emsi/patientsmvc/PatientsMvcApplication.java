2254e3548930e28272ecaf614c3de3a7f4fe2d5f
El Mehdi Cherai <mehdi.cherai@gmail.com>
2022-04-26 20:16:59 +0000

    Initial commit

---
+++ src/main/java/ma/emsi/patientsmvc/PatientsMvcApplication.java
@@ -0,0 +1,40 @@
+package ma.emsi.patientsmvc;
+
+import ma.emsi.patientsmvc.entities.Patient;
+import ma.emsi.patientsmvc.repositories.PatientRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Date;
+
+@SpringBootApplication
+public class PatientsMvcApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(PatientsMvcApplication.class, args);
+    }
+
+    //@Bean
+    CommandLineRunner commandLineRunner(PatientRepository patientRepository){
+        return args -> {
+            patientRepository.save(
+                    new Patient(null,"Hassan",new Date(), false, 120 ));
+            patientRepository.save(
+                    new Patient(null,"Mohammed",new Date(), true, 321 ));
+            patientRepository.save(
+                    new Patient(null,"Yasmine",new Date(), true, 650 ));
+            patientRepository.save(
+                    new Patient(null,"Hanae",new Date(), false, 320 ));
+
+            patientRepository.findAll().forEach(p->{
+                System.out.println(p.getNom());
+            });
+        };
+    }
+
+}
+
+
